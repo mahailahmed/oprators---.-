ae90890048a13312c90ec425307e882df16b7a9d
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-05 12:23:22 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//mathamatics oprators (+,-,*,/)
+var sum = 10 + 10;
+console.log(sum); //sum
+var multiply = 10 * 10; //multiply
+console.log(multiply);
+var divide = 100 / 10; //devide
+console.log(divide);
+var sub = 50 - 25;
+console.log(sub); //sub
---
+++ main.ts
@@ -0,0 +1,16 @@
+//mathamatics oprators (+,-,*,/)
+
+let sum:number=10+10
+console.log(sum)              //sum
+
+
+let multiply:number=10*10         //multiply
+console.log(multiply)
+
+
+let divide:number=100/10          //devide
+console.log(divide)
+
+
+let sub:number=50-25
+console.log(sub)                 //sub
